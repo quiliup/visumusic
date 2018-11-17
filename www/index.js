077512a4bbe8c051fa9e49d8ee5f2c45758d8674
quiliup <486E4EA3C175B4C3@mytum.de>
2018-11-18 00:24:29 +0100

    cleaned up index.js

--- www/index.js
+++ www/index.js
@@ -3,76 +3,52 @@ import abcjs from "abcjs";
 
 var dataArray;
 var analyser;
-var notes;
-var note_counter;
-var performanceChart;
+var notes; // string
+var note_counter = 0;
 var first_note = true;
-
-
-var dps = 5000;
-
-function init_notes() {
-  notes = "X: 1\nT:visumusic\nM:4/4\nL:1/4\nK:none\n|";
-  console.log('init notes');
-  note_counter = 0;
-}
+var performanceChart = new CanvasJS.Chart("container",
+        {
+            zoomEnabled: false,
+            panEnabled: false,
+            legend: {
+                horizontalAlign: "right",
+                verticalAlign: "center"
+            },
+            axisX: {
+                title: "Frequency",
+                logarithmic: true,
+                logarithmBase: 2
+            },
+            axisY: {
+                includeZero: false,
+                minimum: 0,
+                maximum: 10
+            },
+            data: [],  // random generator below
+        });
 
 function update_notes() {
-  if (first_note) {
-    first_note = false;
-    return;
-  }
-  var freq = wasm.get_max_frequency(analyser);
-  var note = wasm.note_for_frequency(freq);
-  notes += note;
-  note_counter += 1;
-  if (note_counter % 4 == 0) {
-    notes += "|"
-  } else {
-    notes += " "
-  }
-  if (note_counter % 20 == 0) {
-    notes += "\n|"
-  }
+    if (first_note) {
+        first_note = false;
+        return;
+    }
+    var freq = wasm.get_max_frequency(analyser);
+    var note = wasm.note_for_frequency(freq);
+    notes += note;
+    note_counter += 1;
+    if (note_counter % 4 == 0) {
+        notes += "|"
+    } else {
+        notes += " "
+    }
+    if (note_counter % 20 == 0) {
+        notes += "\n|"
+    }
+    abcjs.renderAbc("notation", notes, { scale: 2.0 });
 }
 
-function draw() {
-  //analyser.getFloatFrequencyData(dataArray);
-  //console.log("data: " + dataArray);
-  dataArray = wasm.get_data(analyser);
-  update_notes();
-  abcjs.renderAbc("notation", notes, { scale: 2.0 });
-  setTimeout(draw, 1000);
-}
-
-async function run() {
-  init_notes();
-    analyser = await wasm.setup();
-    dataArray = new Float32Array(analyser.frequencyBinCount);
-    console.log("Setup is ready");
-    //setInterval(wasm.analyse_audio, 1000, analyser);
-    draw();
-    performanceChart = new CanvasJS.Chart("container",
-    {
-        zoomEnabled: false,
-        panEnabled: false,
-        legend: {
-            horizontalAlign: "right",
-            verticalAlign: "center"
-        },
-        axisX: {
-          title: "Frequency",
-          logarithmic: true,
-          logarithmBase: 2
-        },
-        axisY: {
-            includeZero: false
-        },
-        data: [],  // random generator below
-    });
-}
-
-function update(){
+function update_chart(){
+    dataArray = wasm.get_data(analyser);
     var dataPoints = [];
     if (analyser === undefined || performanceChart === undefined)
         return;
@@ -85,5 +61,14 @@ function update(){
     performanceChart.options.data = [{type: "line", dataPoints: dataPoints}];
     performanceChart.render();
 }
+
+async function run() {
+    notes = "X: 1\nT:visumusic\nM:4/4\nL:1/4\nK:none\n|";
+    analyser = await wasm.setup();
+    dataArray = new Float32Array(analyser.frequencyBinCount);
+    console.log("Setup is ready");
+    //setInterval(wasm.analyse_audio, 1000, analyser);
+    setInterval(update_notes, 1000);
+    setInterval(update_chart, 50);
+}
 run();
-setInterval(update, 50);
